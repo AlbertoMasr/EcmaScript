4b084c61a511fac71a095d45cee17b4daacd67b8
Alberto Rivas <albertomasr@hotmail.com>
2023-08-15 10:34:12 -0700

    ES Version 7

---
+++ src/es7/array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(numbers.includes(4))
+console.log(numbers.includes(10))
+
+let names = ['John', 'Bob', 'Mary', 'Joe', 'Jane'];
+
+console.log(names.includes('Mary'))
+console.log(names.includes('mary'))
